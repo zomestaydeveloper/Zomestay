22f35f7f7615ca0081dc433baace5cb69bd6b547
aswanth <aswanthgovidan@gamil.com>
2026-01-04 13:02:27 +0530

    fix folder name error

--- zomes_stay/src/pages/UserProfile.jsx
+++ zomes_stay/src/pages/UserProfile.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
-import userService from "../services/user/userService.js";
+import userService from "../services/User/userService.js";
 
 const UserProfile = () => {
   const navigate = useNavigate();
